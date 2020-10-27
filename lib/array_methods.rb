65cde3ca5cae4a1d87707dd74d8e3a12427fa6e0
Myles <mylesjanik@gmail.com>
2020-10-26 22:12:46 -0700

    commit tree 962ca97d56f73ffa40d1ce20ad218bd34b82ece2

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
+    new_array = array.index(element)
 
+    if new_array == 0
+        return true
+    else
+        return false
+    end
 end
 
 def using_sort(array)
-
+    new_array = array.sort
 end
 
 def using_reverse(array)
-
+    new_array = array.reverse
 end
 
 def using_first(array)
-
+    array.first
 end
 
 def using_last(array)
-
+    array.last
 end
 
 def using_size(array)
-
+    array.size
 end
